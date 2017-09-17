f9bfa1ce9160b6864bf376c8688d83cf534d94c2
CMunkyDev <matt@jcmueller.com>
2017-09-17 00:55:33 -0700

    add basic drawing and erasing capabilities

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,7 +4,11 @@ var squaresPerInch = Number(pixelDensity.value);
 var pixelGrid = document.getElementById("pixel-grid");
 var colorPalette = document.getElementById("color-palette");
 var wholeEditor = document.getElementsByClassName("edit-surround")[0];
-var defaultColor = "fff";
+var defaultColor = "#fff";
+var defaultOutlineColor = "ccc";
+var currentlySelectedColor = "#000";
+var keepColoring = false;
+var keepDeleting = false;
 
 function setGridHeight () {
   var editorHeight = wholeEditor.clientHeight;
@@ -13,15 +17,44 @@ function setGridHeight () {
 }
 setGridHeight();
 
+pixelDensity.addEventListener('change', drawGrid);
+
+pixelGrid.addEventListener('mousedown', function (event) {
+  if (event.shiftKey) {
+    keepDeleting = true;
+  } else {
+    keepColoring = true;
+  }
+  drawDelete(event);
+})
+pixelGrid.addEventListener('mouseup', function (event) {
+  keepColoring = false;
+  keepDeleting = false;
+})
+pixelGrid.addEventListener('mouseover', drawDelete);
+
+function drawDelete(event) {
+  if (keepColoring) {
+    if (event.target.classList[0] === "grid-square") {
+      event.target.style.backgroundColor = currentlySelectedColor;
+      event.target.style.outlineColor = currentlySelectedColor;
+    }
+  } else if (keepDeleting) {
+    if (event.target.classList[0] === "grid-square") {
+      event.target.style.backgroundColor = defaultColor;
+      event.target.style.outlineColor = defaultOutlineColor;
+    }
+  }
+}
 
 var grid = {
   squareRowCol : [],
   squareColor : "#fff",
   borderColor : "#ccc"
-};
+}
 
 function initGrid () {
-  let minSideLength = 96 / 32;
+  let minSideLength = 96 / Number(pixelDensity.lastElementChild.value);
   let maxHeight = window.screen.availHeight;
   let maxWidth = window.screen.availWidth;
   let numRows = Math.floor(maxHeight / minSideLength);
@@ -38,7 +71,7 @@ function initGrid () {
 }
 initGrid();
 
-window.addEventListener('resize', printGrid);
+window.addEventListener('resize', printOnResize);
 //pixelDensity.addEventListener('onchange', changeDensity);
 
 function drawGrid () {
@@ -50,8 +83,6 @@ function drawGrid () {
   let sideLength = 96 / squaresPerInch;
   let numRows = Math.floor(pixelGrid.clientHeight/ sideLength) - 1;
   let numCols = Math.floor(pixelGrid.clientWidth / sideLength) - 1;
-
-
   /*
   <div id="row_#" class=gridrow>
     <div id="row_col"></div>...
@@ -60,11 +91,18 @@ function drawGrid () {
   for (let r = 0; r <= numRows; r++) {
     innerRow = "";
     for (let c = 0; c <= numCols; c++) {
-      innerRow += `<div id=\"${r}_${c}\" class=\"grid-square ${wholeGrid[r][c].color}\" style=\"width:${sideLength}px; height:${sideLength}px\"></div>`
+      innerRow += `<div id=\"${r}_${c}\" class=\"grid-square ${wholeGrid[r][c].color}\" style=\"width: ${sideLength}px; height: ${sideLength}px; background-color: ${defaultColor}; outline-color: ${defaultOutlineColor}\"></div>`
     }
     pixelGrid.innerHTML += `<div id=\"grid-row-${r}\" class=\"grid-row\">${innerRow}</div>`;
   }
 }
+drawGrid();
+
+function printOnResize () {
+  setGridHeight();
+  let poot = setTimeout(printGrid, 10);
+}
+
 function printGrid () {
   setGridHeight();
   drawGrid();
